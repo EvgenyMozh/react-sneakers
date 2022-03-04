8168ee3d4cf5bdf20c7523cd12f262ba20f24b3a
evgenymozh <evgenymozhaev90@yandex.ru>
2022-03-04 14:04:35 +0500

    ref

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     return (
         <header className="d-flex justify-between align-center p-40">
         <Link to="/"><div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="logo" />
+          <img width={40} height={40} src="img/logo.png" alt="logo" />
           <div>
             <h3>REACT SNEAKERS</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -17,14 +17,14 @@ const Header = (props) => {
         </div></Link>
         <ul className="d-flex">
           <li onClick={props.onClickCart} className="mr-30 cu-p">
-            <img width={18} height={18} src="/img/cart.svg" alt="Cart" />
+            <img width={18} height={18} src="img/cart.svg" alt="Cart" />
             <span> {totalPrice} руб.</span>
           </li>
           <li>
-            <Link to="/favorites"><img className="mr-20 cu-p" width={18} height={18} src="/img/favorite.svg" alt="Favorite" /></Link>
+            <Link to="/favorites"><img className="mr-20 cu-p" width={18} height={18} src="img/favorite.svg" alt="Favorite" /></Link>
           </li>
           <li>
-          <Link to="/orders"><img className="mr-20 cu-p" width={18} height={18} src="/img/user.svg" alt="User" /></Link>
+          <Link to="/orders"><img className="mr-20 cu-p" width={18} height={18} src="img/user.svg" alt="User" /></Link>
           </li>
         </ul>
       </header>
